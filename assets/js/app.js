705b76562311964fba52348bbfab788bdc88e3fa
Bruno Michel <bmichel@menfin.info>
2020-12-11 17:11:59 +0100

    Use prism.js to highlight <code>

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,11 @@
 // its own CSS file.
 import "../css/app.css"
 
+// Prism is used for syntax highlighting of <code>
+import Prism from "prismjs"
+import "prism-themes/themes/prism-nord.css"
+Prism.highlightAll()
+
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
