140132e331371fc56610aa67c883c7f03dd2a40a
Bruno Michel <bmichel@menfin.info>
2020-12-16 09:32:08 +0100

    Add a background to the OIDC view

--- lib/tools_web/views/layout_view.ex
+++ lib/tools_web/views/layout_view.ex
@@ -1,3 +1,9 @@
 defmodule ToolsWeb.LayoutView do
   use ToolsWeb, :view
+
+  def view(conn) do
+    conn
+    |> Phoenix.Controller.view_module()
+    |> Phoenix.Template.module_to_template_root(ToolsWeb, "View")
+  end
 end
