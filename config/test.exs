653e315cce0c2d6251dd47eccb03f4631404e36d
Bruno Michel <bmichel@menfin.info>
2024-04-04 09:47:17 +0200

    Update the port for config/test.exs

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ import Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :toolbox, Web.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 5002],
+  http: [ip: {127, 0, 0, 1}, port: 5902],
   secret_key_base: "4J2VrpZSEt4LKurM//MBsI23QGxlK1QNaPvelv1IOeXWsU7ZIUPaOEAhGcFNB7wB",
   server: false
 
